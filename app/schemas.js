297d75b861dfc8bcf956964b1d95fe0a7ee71795
Henry Andrews <henry@cloudflare.com>
2017-05-30 22:28:07 -0700

    Update dependencies, add intro, document more things.
    This updates doca to use the latest json-schema-example-loader 2.0.0.
    It also adds a "getting started" introduction section and a way
    to hook it to the table of contents, and documents a few more
    things in the config file.  The config file now starts uncommented,
    as it has a reasonable default for the headers.

--- app/schemas.js
+++ app/schemas.js
@@ -3,5 +3,9 @@
 import { fromJS } from 'immutable';
 
 export default fromJS([
+  // The "getting started" schema provides table-of-contents links
+  // for the sections in src/client/introduction.js.  You are free
+  // to customize or remove both the schema and the introduction component.
+  require('./getting-started.json'),
 ###schemas###
 ]);
--- app/src/client/main.js
+++ app/src/client/main.js
@@ -1,14 +1,15 @@
 import { connect } from 'react-redux';
-import { App } from 'doca-cf-theme';
+import { App } from 'doca-bootstrap-theme';
+import Introduction from './introduction';
 import config from '../../config';
 
 // this dynamically imports css, less and sass from the "THEME/styles"
 try {
-  const reqCSS = require.context('doca-cf-theme/styles', true, /\.css$/ig);
+  const reqCSS = require.context('doca-bootstrap-theme/styles', true, /\.css$/ig);
   reqCSS.keys().forEach(reqCSS);
-  const reqLESS = require.context('doca-cf-theme/styles', true, /\.less$/ig);
+  const reqLESS = require.context('doca-bootstrap-theme/styles', true, /\.less$/ig);
   reqLESS.keys().forEach(reqLESS);
-  const reqSASS = require.context('doca-cf-theme/styles', true, /\.scss$/ig);
+  const reqSASS = require.context('doca-bootstrap-theme/styles', true, /\.scss$/ig);
   reqSASS.keys().forEach(reqSASS);
 } catch (e) {
   // no theme styles were found
@@ -17,6 +18,7 @@ try {
 const mapStateToProps = state => ({
   schemas: state.schemas,
   config,
+  introduction: Introduction,
 });
 
 const Main = connect(
